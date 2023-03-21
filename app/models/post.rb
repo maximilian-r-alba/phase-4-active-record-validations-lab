40346b2ea62c44287afd58b2d9e568fb2dd20621
Maximilian Alba <albam476@gmail.com>
2023-03-21 19:16:36 -0400

    commit tree 0746bd5dac2f9c35e9c28a5d07b941ba38fb59c5

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, acceptance: { accept: ["Fiction" , "Non-Fiction"] , message: "is not included in the list"}
+    validate :clickbait?
+
+    CLICKBAIT_WORDS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+      ]
+
+    def clickbait?
+        if CLICKBAIT_WORDS.none? {|word| word.match title}
+            errors.add(:title, "must be clickbait")
+        end
+    end
 end
